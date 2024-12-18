4d2843574a182c1e596c4fee1dd8dcaa1c824176
0ver1ay <107620656+0ver1ay@users.noreply.github.com>
2024-12-18 23:43:24 +0300

    Update remove duplicates.py

--- remove duplicates.py
+++ remove duplicates.py
@@ -1,12 +1,9 @@
 def remove_duplicates(file_path):
-    # Читаем содержимое файла
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
-    # Удаляем дубликаты, сохраняя порядок
     unique_lines = list(dict.fromkeys(lines))
-
-    # Перезаписываем файл без дубликатов
+    
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(unique_lines)
 
