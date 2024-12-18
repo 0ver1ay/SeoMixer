ed3f8fd6ad003032bd82d222d25a65e4adf7d3fc
0ver1ay <107620656+0ver1ay@users.noreply.github.com>
2024-12-18 23:42:58 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-# Определяем пути к входному файлу и файлам для каждой модели
+
 input_file_path = 'E:/WORK/Python/SeoMixer/texts/out.txt'  # Файл, в котором ищем строки
 model_replacement = {
     'Сандеро': 'Sandero',
@@ -19,18 +19,18 @@ model_replacement = {
 
 # Функция для записи строк в файл по модели
 def write_model_lines(model):
-    model_file_name = model_replacement[model].lower() + '.txt'  # имя выходного файла
+    model_file_name = model_replacement[model].lower() + '.txt'
     model_file_path = f'E:/WORK/Python/SeoMixer/texts/models/{model_file_name}'
 
     with open(input_file_path, 'r', encoding='utf-8') as input_file, \
          open(model_file_path, 'w', encoding='utf-8') as model_file:
 
         for line in input_file:
-            if model in line:  # Проверяем, содержит ли строка модель
+            if model in line: 
                 model_file.write(line)
 
     print(f"Строки для модели '{model}' записаны в файл: {model_file_path}")
 
-# Перебор всех моделей в словаре
+
 for model in model_replacement.keys():
     write_model_lines(model)
